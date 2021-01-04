84dbae696b4880ba53d9c3aeb979b0e6615e7e79
Sun <syseoz@naver.com>
2021-01-04 22:34:09 +0900

    Initial commit

---
+++ src/main/java/com/study/account/AccountController.java
@@ -0,0 +1,16 @@
+package com.study.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model){
+        model.addAttribute("signUpForm",new SignUpForm());
+        //model.addAttribute(new SignUpForm()); 이름이 같으면 생략 가능
+        return "account/sign-up";
+    }
+}

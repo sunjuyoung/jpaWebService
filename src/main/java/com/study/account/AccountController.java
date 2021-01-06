5f3507e17ebef6e46b79ec59de9350c454ab9b2a
Sun <syseoz@naver.com>
2021-01-06 20:42:02 +0900

    회원가입 서브밋, 검증

--- src/main/java/com/study/account/AccountController.java
+++ src/main/java/com/study/account/AccountController.java
@@ -1,16 +1,42 @@
 package com.study.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
         model.addAttribute("signUpForm",new SignUpForm());
         //model.addAttribute(new SignUpForm()); 이름이 같으면 생략 가능
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid @ModelAttribute SignUpForm signUpForm, Errors errors){
+        if(errors.hasErrors()){
+            return "account/sign-up";
+        }
+
+        return "redirect:/";
     }
 }

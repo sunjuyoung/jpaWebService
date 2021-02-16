07ac4ca3b5fe55aa7e62385bdfdf54c9d3694f45
Sun <syseoz@naver.com>
2021-02-16 22:36:27 +0900

    add password modify

--- src/main/java/com/study/settings/Profile.java
+++ src/main/java/com/study/settings/Profile.java
@@ -3,22 +3,30 @@ package com.study.settings;
 import com.study.domain.Account;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor//NullPointerException
 public class Profile {
+
+    @Length(max=40)
     private String bio;                //프로필 한줄소개
 
     private String url;
 
+    @Length(max=60)
     private String occupation;
 
-    private String location;            //varchar(255)
+    @Length(max=60)
+    private String location;
+
+    private String profileImage;
 
     public Profile(Account account){
         this.bio = account.getBio();
         this.url = account.getUrl();
         this.occupation = account.getOccupation();
         this.location = account.getLocation();
+        this.profileImage = account.getProfileImage();
     }
 }
--- src/main/java/com/study/settings/SettingsController.java
+++ src/main/java/com/study/settings/SettingsController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -17,6 +19,12 @@ import javax.validation.Valid;
 @Controller
 @RequiredArgsConstructor
 public class SettingsController {
+
+
+    @InitBinder("passwordForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(new PasswordFormValidator());
+    }
 
     private final AccountService accountService;
 
@@ -39,5 +47,26 @@ public class SettingsController {
         redirectAttributes.addFlashAttribute("message","프로필을 수정하였습니다.");
         return "redirect:/settings/profile";
 
+    }
+
+    @GetMapping("/settings/password")
+    public String passwordUpdateForm(@CurrentUser Account account, Model model){
+        model.addAttribute(account);
+        model.addAttribute(new PasswordForm());
+        return "settings/password";
+    }
+
+    @PostMapping("/settings/password")
+    public String updatePassword(@CurrentUser Account account, @Valid PasswordForm passwordForm, Errors errors,
+                                 RedirectAttributes redirectAttributes, Model model){
+
+        if(errors.hasErrors()){
+            model.addAttribute(account);
+            return "settings/password";
+        }
+
+        accountService.updatePassword(account,passwordForm.getNewPassword());
+        redirectAttributes.addFlashAttribute("message","패스워드 변경완료했습니다.");
+        return "redirect:/settings/password";
     }
 }
